a96debdd7ca4d439acb22eb8f3b04e2c17f3c528
Daniel Ionut <dany89yt@yahoo.com>
2021-12-03 15:15:24 +0200

    clean assets

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from .models import User, Profile
+from .models import User
 
 
-class ProfileInlne(admin.StackedInline):
-    model = Profile
 
 
 class UserAdmin(admin.ModelAdmin):
     model = User
-    inlines = (ProfileInlne,)
 
 
 admin.site.register(User, UserAdmin)
